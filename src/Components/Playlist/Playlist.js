ce7904aa779903700ca2f66bada16f799048642d
ImCalicoJack <roush757@gmail.com>
2022-02-01 14:05:55 -0500

    Spotify playlist app

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Playlist.css'
+import { TrackList } from '../TrackList/TrackList'
+
+export class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleNameChange(event) {
+        this.props.onNameChange(event.target.value);
+    }
+
+    handleClick(event) {
+        this.props.onSave(event.target.value);
+    }
+
+    render() {
+        return(
+            <div className="Playlist">
+                <input value={this.props.playlistName} placeholder='"New Playlist"' onChange={this.handleNameChange} />
+                <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
+                <button className="Playlist-save" onClick={this.handleClick}>SAVE TO SPOTIFY</button>
+            </div>
+        );
+    }
+}
